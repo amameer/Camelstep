0a9f23fb70e9a19eaf9bc5b382363922682cac1b
amnamn12 <amn.alameer@gmail.com>
2017-04-15 11:34:34 +0300

    first commit

---
+++ app/src/main/java/com/misk/camelstep/camelstep/welcomeActivity.java
@@ -0,0 +1,13 @@
+package com.misk.camelstep.camelstep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class welcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
+}
